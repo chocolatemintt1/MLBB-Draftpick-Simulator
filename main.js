43ddba166047742ec5bbe441e2d2242abfd5df97
Carlo <mangxander22@gmail.com>
2024-10-21 16:35:56 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,11 @@ import { teamStrategies } from './data/teamStrategies.js';
 import { GameState } from './modules/GameState.js';
 import { DraftLogic } from './modules/DraftLogic.js';
 import { UI } from './modules/UI.js';
+import { PopupManager } from './modules/PopupManager.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const gameState = new GameState();
     const draftLogic = new DraftLogic(gameState, heroes, teamStrategies);
     const ui = new UI(gameState, draftLogic, teamStrategies, roleOrder);
+    const popupManager = new PopupManager();
 });
