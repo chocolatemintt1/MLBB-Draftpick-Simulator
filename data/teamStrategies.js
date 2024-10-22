76ce5009086e3a5190e0187b9f399780f51b94aa
Carlo <mangxander22@gmail.com>
2024-10-23 05:20:09 +0800

    Update teamStrategies.js

--- data/teamStrategies.js
+++ data/teamStrategies.js
@@ -1,16 +1,16 @@
 export const teamStrategies = {
     blacklist: {
         name: 'Blacklist International',
-        preferredPicks: ['Estes', 'Mathilda', 'Beatrix', 'Valentina', 'Khufra', 'Lancelot', 'Paquito', 'Esmeralda', 'Julian', 'Kagura'],
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou'],
         commonBans: ['Fanny', 'Ling', 'Hayabusa', 'Suyou', 'Alpha'],
         playstyle: 'defensive',
         lateGameFocus: true,
         rolePreference: {
-            jungler: ['Alpha', 'Suyou', 'Ling'],
-            midlane: ['Valentina', 'Aurora', 'Yve'],
-            exp: ['Hylos', 'Khaleed', 'Terizla'],
-            roam: ['Mathilda', 'Rafaela', 'Chou'],
-            gold: ['Harith', 'Karrie', 'Moskov']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     echo: {
@@ -20,25 +20,25 @@ export const teamStrategies = {
         playstyle: 'aggressive',
         lateGameFocus: false,
         rolePreference: {
-            jungler: ['Alpha', 'Ling', 'Suyou'],
-            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana'],
-            exp: ['Phoveus', 'Hylos', 'Arlott'],
-            roam: ['Chou', 'Khufra', 'Edith'],
-            gold: ['Claude', 'Karrie', 'Harith']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     onic: {
         name: 'Fanatic ONIC Philippines',
         preferredPicks: ['Hayabusa', 'Julian', 'Atlas', 'Yve', 'Melissa', 'Valentina', 'Esmeralda', 'Beatrix', 'Khufra', 'Chou'],
-        commonBans: ['Julian', 'Harith', 'Claude', 'Hylos', 'Phoveus'],
+        commonBans: ['Moskov', 'Harith', 'Claude', 'Hylos', 'Gatot Kaca'],
         playstyle: 'aggressive',
         lateGameFocus: false,
         rolePreference: {
-            jungler: ['Nolan', 'Julian', 'Alpha'],
-            midlane: ['Yve', 'Valentina', 'Lou Yi'],
-            exp: ['Chou', 'Gatot Kaca', 'Hylos'],
-            roam: ['Hylos', 'Chou', 'Mathilda'],
-            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     ap: {
@@ -48,25 +48,25 @@ export const teamStrategies = {
         playstyle: 'aggressive',
         lateGameFocus: true,
         rolePreference: {
-            jungler: ['Ling', 'Fanny', 'Alpha'],
-            midlane: ['Valentina', 'Julian', 'Lylia', 'Faramis', 'Aurora', 'Vexana'],
-            exp: ['Khaleed', 'Gatot Kaca', 'Phoveus'],
-            roam: ['Mathilda', 'Arlott', 'Minotaur', 'Tigreal'],
-            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     tnc: {
         name: 'TNC',
         preferredPicks: ['Joy', 'Lancelot', 'Franco', 'Yve', 'Melissa', 'Khufra', 'Valentina', 'Paquito', 'Beatrix', 'Atlas'],
         commonBans: ['Ling', 'Mathilda', 'Fredrinn', 'Fanny', 'Julian'],
-        playstyle: 'aggressive',
-        lateGameFocus: false,
+        playstyle: 'defensive',
+        lateGameFocus: true,
         rolePreference: {
-            jungler: ['Joy', 'Lancelot', 'Aamon'],
-            midlane: ['Yve', 'Valentina', 'Xavier'],
-            exp: ['Paquito', 'Chou', 'Yu Zhong'],
-            roam: ['Franco', 'Khufra', 'Atlas'],
-            gold: ['Melissa', 'Beatrix', 'Brody']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     aurora: {
@@ -74,13 +74,13 @@ export const teamStrategies = {
         preferredPicks: ['Aamon', 'Julian', 'Atlas', 'Xavier', 'Melissa', 'Mathilda', 'Esmeralda', 'Beatrix', 'Khufra', 'Arlott'],
         commonBans: ['Ling', 'Fanny', 'Faramis', 'Suyou', 'Valentina'],
         playstyle: 'aggressive',
-        lateGameFocus: true,
+        lateGameFocus: false,
         rolePreference: {
-            jungler: ['Ling', 'Fanny', 'Alpha', 'Suyou'],
-            midlane: ['Valentina', 'Julian', 'Lylia', 'Faramis', 'Aurora', 'Vexana'],
-            exp: ['Khaleed', 'Gatot Kaca', 'Phoveus'],
-            roam: ['Mathilda', 'Arlott', 'Minotaur', 'Tigreal'],
-            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     rsg: {
@@ -88,27 +88,27 @@ export const teamStrategies = {
         preferredPicks: ['Ling', 'Paquito', 'Lolita', 'Yve', 'Karrie', 'Atlas', 'Valentina', 'Esmeralda', 'Beatrix', 'Mathilda'],
         commonBans: ['Fanny', 'Joy', 'Julian', 'Fredrinn', 'Arlott'],
         playstyle: 'aggressive',
-        lateGameFocus: true,
+        lateGameFocus: false,
         rolePreference: {
-            jungler: ['Ling', 'Hayabusa', 'Aamon'],
-            midlane: ['Yve', 'Valentina', 'Xavier'],
-            exp: ['Paquito', 'Esmeralda', 'Yu Zhong'],
-            roam: ['Lolita', 'Atlas', 'Mathilda'],
-            gold: ['Karrie', 'Beatrix', 'Melissa']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     },
     omg: {
         name: 'Smart Omega',
         preferredPicks: ['Hayabusa', 'Chou', 'Franco', 'Xavier', 'Beatrix', 'Khufra', 'Valentina', 'Julian', 'Melissa', 'Mathilda'],
         commonBans: ['Ling', 'Fanny', 'Joy', 'Fredrinn', 'Arlott'],
-        playstyle: 'aggressive',
-        lateGameFocus: false,
+        playstyle: 'defensive',
+        lateGameFocus: true,
         rolePreference: {
-            jungler: ['Hayabusa', 'Julian', 'Lancelot'],
-            midlane: ['Xavier', 'Valentina', 'Yve'],
-            exp: ['Chou', 'Paquito', 'Esmeralda'],
-            roam: ['Franco', 'Khufra', 'Mathilda'],
-            gold: ['Beatrix', 'Melissa', 'Karrie']
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
         }
     }
 };
