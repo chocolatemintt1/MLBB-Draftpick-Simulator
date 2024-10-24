fcb0234cf4a66a7d5301f05711e7c0505e7c8298
Carlo <mangxander22@gmail.com>
2024-10-24 23:10:43 +0800

    Add files via upload

---
+++ beta-page/data/teamStrategies.js
@@ -0,0 +1,114 @@
+export const teamStrategies = {
+    blacklist: {
+        name: 'Blacklist International',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: [],
+        playstyle: 'defensive',
+        lateGameFocus: true,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    echo: {
+        name: 'Team Liquid Echo Philippines',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: [],
+        playstyle: 'aggressive',
+        lateGameFocus: false,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    onic: {
+        name: 'Fanatic ONIC Philippines',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: [],
+        playstyle: 'aggressive',
+        lateGameFocus: true,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    ap: {
+        name: 'Falcons AP Bren',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: [],
+        playstyle: 'aggressive',
+        lateGameFocus: true,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    tnc: {
+        name: 'TNC',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: ['Gatot Kaca', 'Hylos', 'Hayabusa', 'Suyou', 'Alpha', 'Arlott', 'Khaleed', 'Fanny', 'Moskov', 'Mathilda', 'Aurora'],
+        playstyle: 'defensive',
+        lateGameFocus: true,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    aurora: {
+        name: 'Aurora MLBB',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: ['Gatot Kaca', 'Hylos', 'Hayabusa', 'Suyou', 'Alpha', 'Arlott', 'Khaleed', 'Fanny', 'Moskov', 'Mathilda', 'Aurora'],
+        playstyle: 'aggressive',
+        lateGameFocus: false,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    rsg: {
+        name: 'RSG Philippines',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: [],
+        playstyle: 'aggressive',
+        lateGameFocus: false,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    },
+    omg: {
+        name: 'Smart Omega',
+        preferredPicks: ['Ling', 'Mathilda', 'Hayabusa', 'Valentina', 'Gatot Kaca', 'Lancelot', 'Paquito', 'Alpha', 'Julian', 'Suyou', 'Arlott', 'Fanny', 'Moskov', 'Harith', 'Aurora', 'Valentina', 'Novaria', 'Claude', 'Nolan', 'Fredrinn'],
+        commonBans: [],
+        playstyle: 'defensive',
+        lateGameFocus: true,
+        rolePreference: {
+            jungler: ['Alpha', 'Suyou', 'Ling', 'Fanny', 'Hayabusa'],
+            midlane: ['Valentina', 'Aurora', 'Yve', 'Vexana', 'Luo Yi', 'Lylia'],
+            exp: ['Hylos', 'Khaleed', 'Terizla', 'Phoveus', 'Gatot Kaca', 'Arlott'],
+            roam: ['Mathilda', 'Rafaela', 'Chou', 'Hylos', 'Gatot Kaca', 'Khaleed', 'Arlott'],
+            gold: ['Harith', 'Beatrix', 'Karrie', 'Claude', 'Moskov']
+        }
+    }
+};
